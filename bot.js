a0bac340280546b8afd1e95cda1d6717023af73b
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-01-30 19:29:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,34 +26,22 @@ client.user.setGame(``,'https://www.twitch.tv/tarikrs');                        
 
 
 
-client.on('message', message => {
+	  
+	  client.on('message', message => {
+    if (!message.guild) return;
     if (message.content.startsWith("رابط")) {
 
-  message.channel.createInvite({
+        message.channel.createInvite({
         thing: true,
-        maxUses: 100,
+        maxUses: 5,
         maxAge: 86400
     }).then(invite =>
       message.author.sendMessage(invite.url)
     )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
----------------------
--[${message.guild.name}]  هذا هو رابط سيرفر
----------------------
--هذا الرابط صالح ل 100 مستخدم فقط
----------------------
--هذا الرابط صالح لمده 24 ساعه فقط
----------------------
-**`)
-      message.author.sendEmbed(Embed11)
+  message.channel.send(`** تم أرسال الرابط برسالة خاصة **`)
+
+      message.author.send(`**مدة الرابط : يـوم
+ عدد استخدامات الرابط : 5 **`)
     }
 });
 
