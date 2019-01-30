c4680a7d42e66671ce86b4c2d88e30f27b515622
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-01-30 19:22:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -341,3 +341,20 @@ client.on('message', message => {
     }
 });
 
+
+
+
+
+
+
+client.on("guildMemberAdd", member => {
+    member.createDM().then(function (channel) {
+    return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+  :crown:اسم العضو  ${member}:crown:  
+  انت العضو رقم ${member.guild.memberCount} `) 
+  }).catch(console.error)
+  })
+
+
+
+
