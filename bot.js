21989ec20fe30a931eb3e30029621c156bcf3334
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-02-02 17:41:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,14 @@ client.on("message", (message) => {
 
 
 
-
+const replyForMention = [    "ايش تبي",    "سوي +help",    "احد ناداني؟",    "سم؟",    "كيفك",    "تمنشن بوت ياغبي؟",    "لا تزعجني",    "هلا"]
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply(replyForMention);
+    }
+});
 
 
 
