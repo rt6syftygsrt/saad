17e827cf954e1e2e751f1e374568176e197db87f
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-02-03 09:55:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,24 @@ client.on('ready', () => {
 
 
 
+          
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = ['SMG ',' | Welcome ' ];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j
+        client.user.setGame(setGame[i],`https://www.twitch.tv/حب بلا حدود`);
+    }, ms);
 
+})
 
 
 
