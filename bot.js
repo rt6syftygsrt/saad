e19adcbacfc9e859023565e1b0608bd4760a3eb9
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-02-03 09:28:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -827,12 +827,13 @@ client.on('channelUpdate', (oldChannel, newChannel) => {
 
 
 
+
 Malicioustm copyright arrow_down            
 client.on('message', message =>{
     if(message.author.bot) return;
-    if(!message.content == ('مسح'))
+    if(!message.content == ('>clear'))
 if(!true) return;
-    if(message.content.split(' ')[0] == ('مسح')){
+    if(message.content.split(' ')[0] == ('>clear')){
     var lmt = message.content.split(' ')[1]
     ,  hang = 0
     ,  max  = 0;
@@ -854,7 +855,4 @@ if(!true) return;
         message.channel.fetchMessages({limite:hang}).then(msgs=>{
         message.channel.bulkDelete(msgs);
      });
-
-
-
 
