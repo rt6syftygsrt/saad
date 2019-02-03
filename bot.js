22f8b8dcae8ea223c1499d16054b09c576cdd1ea
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-02-03 10:15:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,34 +26,47 @@ client.user.setGame(`$help|welcome`,'https://www.twitch.tv/tarikrs');           
 
 
 
-
-
-
-
-
-
-client.on('message', msg => {
-  const DiscordLink = [".net", ".tk", ".com"];///!                  Mal,SizGooo ツ
-  if( DiscordLink.some(word => msg.content.includes(word)) ) {
-    msg.reply(`**حبي ممنوع نشر اي روابط😂 **`)
-    msg.delete();///!                  Mal,SizGooo ツ
-
-}
-})///!                  Mal,SizGooo ツ
-
-
-
-
+Toxic Codescopyright arrow_down
+client.on('message', message => {
+    var prefix = "#";
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
          
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.get('538911913627615253').roles.find('name', 'clear').edit({color: 'RANDOM'})
-    },20000);
-  /// https://discord.gg/yzBdF3E /// Mal , Codes //// Mal , Galal
-/// https://discord.gg/yzBdF3E /// Mal , Codes //// Mal , Galal 
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  if (message.mentions.users.size < 1) return message.channel.send(`https://cdn.pg.sa/fjxlms81nk.png`);
+  if(!reason) return message.channel.send(`https://cdn.pg.sa/fjxlms81nk.png`);
+  if (!message.guild.member(user)
+  .bannable) return message.reply(`This User Is Have High Role !`);
+ 
+  message.guild.member(user).ban(7, user);
+ 
+  const banembed = new Discord.RichEmbed()
+  .setAuthor(`BANNED!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : banembed
+  })
+}
+});
 
 
-})
+
+          
 
 
 
