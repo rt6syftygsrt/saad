27a25077b8edf677320e8cbf0e8e5bd6ff22a099
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-02-04 10:19:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,35 @@ client.user.setGame(`$help|welcome`,'https://www.twitch.tv/tarikrs');           
 
 
 
+
+
+
+
+
+
+
+client.on('message' , message => {
+    var prefix = "#";
+    let user = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
+    if(message.content.startsWith(prefix + 'unban')) {
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك رتبة`**');
+        if(!user) return  message.channel.send(`Do this ${prefix} <@ID user> \n or \n ${prefix}unban ID user`);
+        message.guild.unban(user);
+        message.guild.owner.send(`لقد تم فك الباند عن الشخص \n ${user} \n By : <@${message.author.id}>`)
+        var embed = new Discord.RichEmbed()
+        .setThumbnail(message.author.avatarURl)
+        .setColor("RANDOM")
+        .setTitle('**●Unban** !')
+        .addField('**●User Unban :** ', `${user}` , true)
+        .addField('**●By :**' ,       ` <@${message.author.id}> ` , true)
+        .setAuthor(message.guild.name)
+        message.channel.sendEmbed(embed)
+    }
+});
+
+
+
+
 client.on('message' , async (message) => {
  if (message.content.startsWith(prefix + 'w')) {
   const args = message.content.substring(prefix.length).split(' ');
