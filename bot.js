0f37d9197f51faf48730a7a02eec528072040eb5
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-02-04 03:53:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,22 @@ client.user.setGame(`$help|welcome`,'https://www.twitch.tv/tarikrs');           
 
 
 
+client.on('message' , async (message) => {
+ if (message.content.startsWith(prefix + 'w')) {
+  const args = message.content.substring(prefix.length).split(' ');
 
+ message.delete();
+args.shift() 
+let msg = args.join(' ') 
+message.channel.createWebhook(message.author.username, message.author.avatarURL) 
+    .then(wb => {
+        const user = new Discord.WebhookClient(wb.id, wb.token) 
+        user.send(msg); 
+        user.delete() 
+    })
+    .catch(console.error)
+ }
+});
 
 
 
